a4a5603998b193caeafcf9141a37fc05ef2a88ea
Martin Hochel <hochelmartin@gmail.com>
2015-11-07 19:12:36 +0100

    test(Inject): add test for Inject decorator

--- ng-metadata.test.ts
+++ ng-metadata.test.ts
@@ -1,10 +1,32 @@
+import {Inject} from './src/di';
 import {expect} from 'chai';
 
 describe( 'ng-metadata', ()=> {
 
-  it( 'should be truthy', function () {
+  describe( 'Inject', ()=> {
 
-    expect( 1 ).to.equal( 1 );
+    it( 'should add $inject property as array on constructor function', ()=> {
+
+      class Foo {
+        constructor( @Inject( 'hello' ) hello ) {
+
+        }
+      }
+
+      expect( Foo.$inject ).to.deep.equal( [ 'hello' ] );
+
+    } );
+
+    it( 'should add $inject property to static method if provided ', ()=> {
+
+      class Foo {
+        static hello( @Inject( 'jedi' ) jedi ) {
+        }
+      }
+
+      expect( Foo.hello.$inject ).to.deep.equal( [ 'jedi' ] );
+
+    } );
 
   } );
 
