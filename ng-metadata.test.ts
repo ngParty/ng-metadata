df9d1acd220b1165754f71db2bfb729b84527aae
Martin Hochel <hochelmartin@gmail.com>
2015-11-07 19:18:52 +0100

    chore(test): move tests to separate folder

--- ng-metadata.test.ts
+++ ng-metadata.test.ts
@@ -1,33 +1,6 @@
-import {Inject} from './src/di';
 import {expect} from 'chai';
+import './test/di.spec';
 
 describe( 'ng-metadata', ()=> {
-
-  describe( 'Inject', ()=> {
-
-    it( 'should add $inject property as array on constructor function', ()=> {
-
-      class Foo {
-        constructor( @Inject( 'hello' ) hello ) {
-
-        }
-      }
-
-      expect( Foo.$inject ).to.deep.equal( [ 'hello' ] );
-
-    } );
-
-    it( 'should add $inject property to static method if provided ', ()=> {
-
-      class Foo {
-        static hello( @Inject( 'jedi' ) jedi ) {
-        }
-      }
-
-      expect( Foo.hello.$inject ).to.deep.equal( [ 'jedi' ] );
-
-    } );
-
-  } );
 
 } );
---
+++ test/di.spec.ts
@@ -0,0 +1,29 @@
+import {expect} from 'chai';
+import {Inject} from '../src/di';
+
+describe( 'Inject', ()=> {
+
+  it( 'should add $inject property as array on constructor function', ()=> {
+
+    class Foo {
+      constructor( @Inject( 'hello' ) hello ) {
+
+      }
+    }
+
+    expect( Foo.$inject ).to.deep.equal( [ 'hello' ] );
+
+  } );
+
+  it( 'should add $inject property to static method if provided ', ()=> {
+
+    class Foo {
+      static hello( @Inject( 'jedi' ) jedi ) {
+      }
+    }
+
+    expect( Foo.hello.$inject ).to.deep.equal( [ 'jedi' ] );
+
+  } );
+
+} );
