9f8f688d4f82887042ca2f78c6540e4431ec28f9
Martin Hochel <hochelmartin@gmail.com>
2015-11-07 20:27:40 +0100

    test(utils): add tests for util public members

--- ng-metadata.test.ts
+++ ng-metadata.test.ts
@@ -1,5 +1,6 @@
 import {expect} from 'chai';
 import './test/di.spec';
+import './test/util.spec';
 
 describe( 'ng-metadata', ()=> {
 
---
+++ test/util.spec.ts
@@ -0,0 +1,86 @@
+import {expect} from 'chai';
+import {makeSelector,stringify,hasInjectables,provide} from '../src/util';
+
+describe( 'utils', ()=> {
+
+  describe( 'stringify', ()=> {
+
+    it( 'should return name property if it exist on provided type', function () {
+
+      function foo() {
+      }
+
+      class Moo {
+      }
+
+      expect( stringify( foo ) ).to.equal( 'foo' );
+      expect( stringify( Moo ) ).to.equal( 'Moo' );
+
+    } );
+
+    it( 'should return string of provided type if it doesnt has name', function () {
+
+      const obj = { hello: 'world' };
+
+      expect( stringify( function () {} ) ).to.equal( `function () { }` );
+      expect( stringify( obj ) ).to.equal( '[object Object]' );
+
+    } );
+
+  } );
+  describe( 'hasInjectables', ()=> {
+
+    it( 'should check if Type has $inject as array and its not empty', function () {
+
+      class Moo {
+        static $inject = [ 'hello' ];
+      }
+      class NoMames {
+        static $inject = [];
+      }
+      const obj = { $inject: null };
+      const name = 'jabba';
+
+      expect( hasInjectables( Moo ) ).to.equal( true );
+      expect( hasInjectables( NoMames ) ).to.equal( false );
+      expect( hasInjectables( obj ) ).to.equal( false );
+      expect( hasInjectables( name ) ).to.equal( false );
+
+    } );
+
+
+  } );
+  describe( 'makeSelector', ()=> {
+
+
+    it( 'should accept element selector and create camelCase from it', function () {
+
+      const selector = 'hello-world';
+      expect( makeSelector( selector ) ).to.equal( 'helloWorld' );
+
+    } );
+    it( 'should accept attribute selector and create camelCase from it', function () {
+
+      const selector = '[im-your-father]';
+      expect( makeSelector( selector ) ).to.equal( 'imYourFather' );
+
+    } );
+    it( 'should throw error when not valid element or attribute selector provided', function () {
+
+      let selector = 'yabba daba';
+
+      function willThrow() {
+        return makeSelector( selector )
+      }
+
+      expect( willThrow ).to
+        .throw( 'Only selectors matching element names or base attributes are supported, got: yabba daba' );
+
+    } );
+  } );
+  describe( 'provide', ()=> {
+
+
+  } );
+
+} );
