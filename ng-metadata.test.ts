bd4f6034b87d00bf4726e2a436fed163dc90e2a8
Martin Hochel <hochelmartin@gmail.com>
2016-01-03 21:44:36 +0100

    feat(pipes/pipe_provider): create pipe provider for creating angular.filter with correct name for ng container

--- ng-metadata.test.ts
+++ ng-metadata.test.ts
@@ -1,20 +1,23 @@
-import { expect } from 'chai';
-import './test/host.spec';
-import './test/inject.spec';
-import './test/injectable.spec';
-import './test/util.spec';
-import './test/directives.spec';
-import './test/bootstrap.spec';
-import './test/pipe.spec';
-import './test/life_cycle.spec';
-import './test/providers.spec';
+//import { expect } from 'chai';
+
+//import './test/host.spec';
+//import './test/inject.spec';
+//import './test/injectable.spec';
+//import './test/util.spec';
+//import './test/directives.spec';
+//import './test/bootstrap.spec';
+//import './test/pipe.spec';
+//import './test/life_cycle.spec';
+//import './test/providers.spec';
 
 import './test/di/decorators.spec';
 import './test/di/povider.spec';
 import './test/util/decorators.spec';
 import './test/reflection/reflection.spec';
 import './test/facade/lang.spec';
+import './test/facade/collections.spec';
+import './test/linker/pipe_resolver.spec';
+import './test/pipes/pipe_provider.spec';
 
 describe( 'ng-metadata', ()=> {
-
 } );
