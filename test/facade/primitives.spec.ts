3093379ec9e0263d09f88742c05b4be1fc2546af
Martin Hochel <hochelmartin@gmail.com>
2016-01-06 11:14:36 +0100

    feat(facade/primitives): StringWrapper with es6 ponyfills

---
+++ test/facade/primitives.spec.ts
@@ -0,0 +1,45 @@
+import {expect} from 'chai';
+import {StringWrapper} from "../../src/facade/primitives";
+
+describe( `facade/primitives`, ()=> {
+
+  describe( `ES6/7 ponyfills`, ()=> {
+
+    it( `should correctly use String.prototype.startsWith`, ()=> {
+
+      const str = 'To be, or not to be, that is the question.';
+
+      expect(StringWrapper.startsWith(str,'To be')).to.equal(true);
+      expect(StringWrapper.startsWith(str,'not to be')).to.equal(false);
+      expect(StringWrapper.startsWith(str,'not to be', 10)).to.equal(true);
+
+    } );
+    it( `should correctly use String.prototype.endsWith`, ()=> {
+
+      const str = 'To be, or not to be, that is the question.';
+
+      expect( StringWrapper.endsWith( str, 'question.' ) ).to.equal( true );
+      expect( StringWrapper.endsWith( str, 'to be' ) ).to.equal( false );
+      expect( StringWrapper.endsWith( str, 'to be', 19 ) ).to.equal( true );
+
+    } );
+    it( `should correctly use String.prototype.includes`, ()=> {
+
+      let actual = StringWrapper.includes( 'Blue Whale', 'blue' );
+      let expected = false;
+
+      expect( actual ).to.equal( expected );
+
+      const str = 'To be, or not to be, that is the question.';
+
+      expect( StringWrapper.includes( str, 'To be' ) ).to.equal( true );
+      expect( StringWrapper.includes( str, 'question' ) ).to.equal( true );
+      expect( StringWrapper.includes( str, 'nonexistent' ) ).to.equal( false );
+      expect( StringWrapper.includes( str, 'To be', 1 ) ).to.equal( false );
+      expect( StringWrapper.includes( str, 'TO BE' ) ).to.equal( false );
+
+    } );
+
+  } );
+
+} );
