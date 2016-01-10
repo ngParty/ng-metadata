402c5c8d5bec75c7e7c651b6b52be3cc9780e165
Martin Hochel <hochelmartin@gmail.com>
2016-01-10 21:33:39 +0100

    feat(platform): create platform with bootstrap fn helper

---
+++ src/platform/browser.ts
@@ -0,0 +1,34 @@
+export type AppRoot = string | Element | Document;
+
+/**
+ * bootstrap angular app
+ * @param {ng.IModule}  ngModule
+ * @param {string | Element | Document} [element=document]
+ * @param {boolean} [strictDi=true]
+ */
+export function bootstrap(
+  ngModule: ng.IModule,
+  {element=document,strictDi=true}: {
+    element?: AppRoot,
+    strictDi?: boolean
+  }={}
+) {
+
+  const appRoot = _getAppRoot( element );
+
+  angular.element( document ).ready( ()=> {
+    angular.bootstrap( appRoot, [ ngModule.name ], {
+      strictDi: true
+    } )
+  } );
+
+}
+
+function _getAppRoot( element: AppRoot ): Element {
+
+  if ( typeof element === 'string' ) {
+    return document.querySelector( element );
+  }
+  return element as Element;
+
+}
