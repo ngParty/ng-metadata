e972d1e0da0d6f1321e535c323622eab18a62d05
Martin Hochel <hochelmartin@gmail.com>
2016-01-17 23:01:12 +0100

    feat(core): expose life cycle interface to public

--- core.ts
+++ core.ts
@@ -2,3 +2,4 @@
 export * from './src/core/di';
 export { Attr, Input, Output, HostBinding, HostListener, Directive, Component } from './src/core/directives';
 export { Pipe, PipeTransform } from './src/core/pipes';
+export * from './src/core/linker';
--- src/core/linker.ts
+++ src/core/linker.ts
@@ -1,1 +1,6 @@
-export { AfterContentInit, OnDestroy, OnInit } from './linker/directive_lifecycle_interfaces';
+export {
+  AfterContentInit,
+  AfterViewInit,
+  OnDestroy,
+  OnInit
+} from './linker/directive_lifecycle_interfaces';
