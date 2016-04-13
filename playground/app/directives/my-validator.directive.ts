3f4ec09fb22a67a08a5c8954279a273531a14ebf
Daniele Morosinotto <d.morosinotto@icloud.com>
2016-04-13 15:21:06 +0200

    chore(playground): strict typecheck for myvalidator.directive

--- playground/app/directives/my-validator.directive.ts
+++ playground/app/directives/my-validator.directive.ts
@@ -18,9 +18,14 @@ export class MyValidatorDirective implements OnInit,AfterContentInit{
   }
   ngAfterContentInit(){
     console.log( this.todoSvc );
-    this.ngModelCtrl.$validators["myValidator"] = (viewValue,modelValue)=>{
+    (this.ngModelCtrl.$validators as ImyValidators).myValidator = (viewValue,modelValue)=>{
       return viewValue ==='Martin';
     }
   }
 
+}
+
+//interface to have strict typecheck for custom myValidator and avoid this.ngModelCtrl.$validators["myValidator"]
+interface ImyValidators extends ng.IModelValidators {
+    myValidator: (modelValue: any, viewValue: any) => boolean
 }
