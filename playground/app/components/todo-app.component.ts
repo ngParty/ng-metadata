b1b25530bc520f726520dd1ecfa2f2c84c205f60
Daniele Morosinotto <d.morosinotto@icloud.com>
2016-04-25 17:21:58 +0200

    feat(playground): add examples with EventEmitter + EventHandler

--- playground/app/components/todo-app.component.ts
+++ playground/app/components/todo-app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from 'ng-metadata/core';
+import { Component, Inject, EventHandler } from 'ng-metadata/core';
 import { TodoModel, TodoStore } from '../stores/todoStore.service';
 
 @Component( {
@@ -44,7 +44,7 @@ export class TodoAppCmp{
   }
 
   markAsDone( todo: TodoModel ) {
-
+    console.log("HANDLE on-done (with a simple method without typed EventHandler)", todo);
     const todoToMarkAsDone = this.todos.filter( (todoItem)=>todoItem.label===todo.label );
     todoToMarkAsDone[0].complete = todo.complete;
 
@@ -70,8 +70,8 @@ export class TodoAppCmp{
     this.items.push(newId);
   }
 
-  outputWithEmitReceiver( sentence: string ) {
-    console.log( sentence );
+  outputWithEmitReceiver: EventHandler<string> = ( sentence ) => {
+    console.info("HANDLE emit-test (explicit EventHandler typed function)",sentence);
   }
 
 
