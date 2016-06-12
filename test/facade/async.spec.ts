3550eff35927beceb04928760a41e94ef73a5881
Martin Hochel <hochelmartin@gmail.com>
2016-06-13 00:05:57 +0200

    feat(facade/async): make EventEmitter real emitter - superclass of Rx/Subject

--- test/facade/async.spec.ts
+++ test/facade/async.spec.ts
@@ -1,50 +1,10 @@
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import { EventEmitter } from '../../src/facade/async';
-import { isArray, isFunction } from '../../src/facade/lang';
 
 describe( `facade/async`, () => {
 
   describe( `EventEmitter`, () => {
-
-    describe( `static helpers`, () => {
-
-      it( `should extend function by event emitter properties/functions`, () => {
-
-        function myCallback() {}
-
-        EventEmitter.makeNgExpBindingEmittable( myCallback );
-
-        expect( isArray( (myCallback as any)._generatorOrNextFn ) ).to.equal( true );
-        expect( isFunction( (myCallback as any).emit ) ).to.equal( true );
-        expect( isFunction( (myCallback as any)._dispose ) ).to.equal( true );
-        expect( isFunction( (myCallback as any).subscribe ) ).to.equal( true );
-
-      } );
-
-      it( `should properly call distinguish if context is instance of EventEmitter or Function and emit`, () => {
-
-        const spy = sinon.spy();
-        const instance = new EventEmitter();
-        const fun = function ( payload ) { spy( payload ) };
-
-        sinon.spy( instance, '_ngExpressionBindingCb' );
-
-        ( EventEmitter as any ).createEmit( 'hello', instance );
-
-        expect( ((instance as any)._ngExpressionBindingCb as Sinon.SinonSpy).calledWith( { $event: 'hello' } ) )
-          .to
-          .equal( true );
-
-
-        EventEmitter.makeNgExpBindingEmittable( fun );
-        ( EventEmitter as any ).createEmit( 'yo mama is fat!', fun, fun );
-
-        expect( spy.calledWith( { $event: 'yo mama is fat!' } ) ).to.equal( true );
-
-      } );
-
-    } );
 
     describe( `instance`, () => {
 
@@ -70,7 +30,7 @@ describe( `facade/async`, () => {
 
         const sideEffectSpy = sinon.spy();
 
-        const disposable = output.subscribe( sideEffectSpy );
+        const subscription = output.subscribe( sideEffectSpy );
 
         output.emit( 'han solo' );
 
@@ -80,7 +40,7 @@ describe( `facade/async`, () => {
 
         expect( sideEffectSpy.calledTwice ).to.equal( true );
 
-        disposable();
+        subscription.unsubscribe();
 
         output.emit( 'han solo' );
 
