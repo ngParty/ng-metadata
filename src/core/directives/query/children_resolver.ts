47d32f9ca60340632f0a4c96304a33071410f8f8
James Henry <james@henry.sc>
2016-10-16 13:12:13 +0100

    Use as syntax for Type coercion

--- src/core/directives/query/children_resolver.ts
+++ src/core/directives/query/children_resolver.ts
@@ -323,7 +323,7 @@ export function _getParentCheckNotifiers( ctrl: DirectiveCtrl, requiredCtrls: Ob
               return false;
             }
             // check if current child is really queried from its parent
-            return ctrl instanceof <Type>propMetaInstance.selector;
+            return ctrl instanceof (propMetaInstance.selector as Type);
 
           } )
           .forEach( ( propMetaInstance: QueryMetadata )=> {
