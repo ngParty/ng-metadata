947078c5d5699a22d52991dc902c4522d9a3cc0f
Martin Hochel <hochelmartin@gmail.com>
2016-11-20 21:43:29 +0100

    feat(playground): add example for preventing user input via directive HostListener

--- playground/app/components/attribute-directive/attribute-directive.module.ts
+++ playground/app/components/attribute-directive/attribute-directive.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from 'ng-metadata/core';
 
-import { AttributeDirectiveExampleComponent } from './attribute-directive-example.component';
+import { AttributeDirectiveExampleComponent, PreventKeydownDirective } from './attribute-directive-example.component';
 import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   imports: [],
   // exports, bootstrap, entryComponents have no real functionality yet
   exports: [AttributeDirectiveExampleComponent],
-  declarations: [AttributeDirectiveExampleComponent, HighlightDirective]
+  declarations: [
+    AttributeDirectiveExampleComponent,
+    HighlightDirective,
+    PreventKeydownDirective
+  ]
 })
 export class AttributeDirectiveModule{}
